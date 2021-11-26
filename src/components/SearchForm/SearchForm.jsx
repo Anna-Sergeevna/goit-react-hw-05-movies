dd3f18541f80c254a1a8139359ea4d75173a3b2e
Anna-Sergeevna <4042808@gmail.com>
2021-11-26 22:48:42 +0200

    Add toastify

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,45 +1,46 @@
-// import { useState } from 'react';
+import { useState } from 'react';
+import { toast } from 'react-toastify';
 import s from './SearchForm.module.css';
 
-function SearchForm() {
-  // const [query, setQuery] = useState('');
+function SearchForm({ onSubmit }) {
+  const [query, setQuery] = useState('');
 
-  // const handleChange = e => {
-  //   setQuery(e.currentTarget.value.toLowerCase());
-  // };
+  const handleChange = e => {
+    setQuery(e.currentTarget.value.toLowerCase());
+  };
 
-  // const handleSubmit = e => {
-  //   e.preventDefault();
+  const handleSubmit = e => {
+    e.preventDefault();
 
-  //   if (query.trim() === '') {
-  //     // toast.error('Enter the queryn');
-  //     setQuery('');
-  //     return;
-  //   }
+    if (query.trim() === '') {
+      toast.error('Enter the queryn');
+      setQuery('');
+      return;
+    }
 
-  //   onSubmit(query);
-  //   reset();
-  // };
+    onSubmit(query);
+    reset();
+  };
 
-  // const reset = () => {
-  //   setQuery('');
-  // };
+  const reset = () => {
+    setQuery('');
+  };
 
   return (
-    // <form className={s.searchForm} onSubmit={handleSubmit}>
-    <form className={s.searchForm}>
+    <form className={s.searchForm} onSubmit={handleSubmit}>
+      {/* // <form className={s.searchForm}> */}
       <input
         className={s.searchFormInput}
         type="text"
         autoComplete="off"
         autoFocus
         placeholder="Search movies"
-        // value={query}
-        // onChange={handleChange}
+        value={query}
+        onChange={handleChange}
       />
 
-      {/* <button type="submit" disabled={!query} className={s.searchFormButton}> */}
-      <button type="submit" className={s.searchFormButton}>
+      <button type="submit" disabled={!query} className={s.searchFormButton}>
+        {/* <button type="submit" className={s.searchFormButton}> */}
         <span className={s.searchFormButtonLabel}>Search</span>
       </button>
     </form>
--- src/views/MoviesPage/MoviesPage.jsx
+++ src/views/MoviesPage/MoviesPage.jsx
@@ -1,10 +1,21 @@
 import SearchForm from '../../components/SearchForm';
 import s from './MoviesPage.module.css';
+import { useState } from 'react';
+
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function MoviesPage() {
+  const [query, setQuery] = useState('');
+
+  const onSubmit = query => {
+    setQuery(query);
+  };
+
   return (
     <div className={s.moviesPage}>
-      <SearchForm />
+      <SearchForm onSubmit={onSubmit} query={query} />
+      <ToastContainer autoClose={3000} />
     </div>
   );
 }
