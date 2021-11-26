79f3a298e835777c5dec25d198964a2a866e2a31
Anna-Sergeevna <4042808@gmail.com>
2021-11-26 14:45:08 +0200

    add start files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Container from 'components/Container';
+import './App.css';
+
+function App() {
+  return <Container title=""></Container>;
+}
+
+export default App;
+
+// '/' - компонент <HomePage>, домашняя страница со списком популярных кинофильмов.
+// '/movies' - компонент <MoviesPage>, страница поиска фильмов по ключевому слову.
+// '/movies/:movieId' - компонент <MovieDetailsPage>, страница с детальной информацией о кинофильме.
+// /movies/:movieId/cast - компонент <Cast>, информация о актерском составе. Рендерится на странице <MovieDetailsPage>.
+// /movies/:movieId/reviews - компонент <Reviews>, информация об обзорах. Рендерится на странице <MovieDetailsPage>.
