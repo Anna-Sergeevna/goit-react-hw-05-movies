0080e6ee238d119eaf5baeb3bf266dec272b8962
Anna-Sergeevna <4042808@gmail.com>
2021-11-29 16:48:18 +0200

    Add Genres & default poster for Movie

--- src/views/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/views/MovieDetailsPage/MovieDetailsPage.jsx
@@ -3,12 +3,13 @@ import { NavLink, useParams, Outlet } from 'react-router-dom';
 
 import * as fetchMovieAPI from '../../services/movieApi';
 import { URL } from '../../services/settings-url';
+import defaultPoster from './defaultPoster.jpg';
 
 import s from './MovieDetailsPage.module.css';
 
 function MovieDetailsPage() {
   const { movieId } = useParams();
-  const [movie, setMovie] = useState({});
+  const [movie, setMovie] = useState(null);
 
   useEffect(() => {
     fetchMovieAPI.fetchMovieById(movieId).then(setMovie);
@@ -20,18 +21,29 @@ function MovieDetailsPage() {
         <>
           <div className={s.movieCard}>
             <img
-              src={`${URL.IMAGE}${movie.poster_path}`}
+              src={
+                movie.poster_path
+                  ? `${URL.IMAGE}${movie.poster_path}`
+                  : defaultPoster
+              }
               alt={movie.title || movie.name}
             />
             <div className={s.description}>
-              <h2 className={s.title}>{movie.title || movie.name}</h2>
-              <h3 className={s.title}>Genres???</h3>
+              <h2 className={s.titleMovie}>{movie.title || movie.name}</h2>
+              <h3 className={s.title}>Genres</h3>
+              <ul>
+                {movie.genres.map(({ id, name }) => (
+                  <li className={s.infoItem} key={id}>
+                    {name}
+                  </li>
+                ))}
+              </ul>
+              <h3 className={s.title}>Overview </h3>
+              <p className={s.info}>{movie.overview}</p>
               <h3 className={s.title}>Score </h3>
               <p className={s.info}>{movie.vote_average}</p>
               <h3 className={s.title}>Votes </h3>
               <p className={s.info}>{movie.vote_count}</p>
-              <h3 className={s.title}>Overview </h3>
-              <p className={s.info}>{movie.overview}</p>
               <h3 className={s.title}>Runtime </h3>
               <p className={s.info}>{movie.runtime} min</p>
             </div>
@@ -64,3 +76,7 @@ function MovieDetailsPage() {
 export default MovieDetailsPage;
 
 // data => setMovie(data);
+
+// const [movie, setMovie] = useState({});
+
+// ({movie.release_date.slice(0, 4)})
